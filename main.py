5ce707828b373ffaa7824d04fd323835cbb8e0f9
rahul <rahulmuraly29@gmail.com>
2025-10-22 14:58:33 +0530

    intial

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from app.routes.transform_route import router as transform_router
+import logging
+from app.config.constants import LOG_FILE
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+app = FastAPI(title="Data Transformation API", version="1.0")
+
+app.include_router(transform_router, prefix="/api")
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to Data Transformation API"}
+
+
